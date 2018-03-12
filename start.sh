3dea28245f67a4a0ce7ddf4d913afa606ebd94e2
Leticiasg <leticiagoncalves2005@gmail.com>
2018-03-12 02:19:28 -0300

    edit start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-exec gunicorn lovelace.wsgi:application \
+exec cd lovelace && gunicorn lovelace.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
